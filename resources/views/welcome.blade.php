fec7af6e79f15a35f57a9cdddfff14f3b8acecde
elianiva <dicha.arkana03@gmail.com>
2024-04-18 18:52:22 +0700

    feat: template form 5-6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,74 +1,170 @@
 @extends('adminlte::page')
-{{-- --}}
-@section('subtitle', 'Welcome')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
 
-@section('content_body')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+@section('title', 'Dashboard')
 
-{{-- Extend and customize the browser title --}}
-@section('title')
-    {{ config('adminlte.title') }}
-    @hasSection('subtitle')
-        | @yield('subtitle')
-    @endif
-@stop
-
-{{-- Extend and customize the page content header --}}
 @section('content_header')
-    @hasSection('content_header_title')
-        <h1 class="text-muted">
-            @yield('content_header_title')
-            @hasSection('content_header_subtitle')
-                <small class="text-dark">
-                    <i class="fas fa-xs fa-angle-right text-muted"></i>
-                    @yield('content_header_subtitle')
-                </small>
-            @endif
-        </h1>
-    @endif
+    <h1>Dashboard</h1>
 @stop
 
-{{-- Rename section content to content_body --}}
 @section('content')
-    @yield('content_body')
-@stop
+    <div class="card card-warning">
+        <div class="card-header">
+            <h3 class="card-title">General Elements</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <form>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- text input -->
+                        <div class="form-group">
+                            <label>Text</label>
+                            <input type="text" class="form-control" placeholder="Enter ...">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Text Disabled</label>
+                            <input type="text" class="form-control" placeholder="Enter ..." disabled>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- textarea -->
+                        <div class="form-group">
+                            <label>Textarea</label>
+                            <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Textarea Disabled</label>
+                            <textarea class="form-control" rows="3" placeholder="Enter ..." disabled></textarea>
+                        </div>
+                    </div>
+                </div>
 
-{{-- Create a common footer --}}
-@section('footer')
-    <div class="float-right">
-        Version: {{ config('app.version', '1.0.0') }}
+                <!-- input states -->
+                <div class="form-group">
+                    <label class="col-form-label" for="inputSuccess"><i class="fas fa-check"></i> Input with
+                        success</label>
+                    <input type="text" class="form-control is-valid" id="inputSuccess" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                    <label class="col-form-label" for="inputWarning"><i class="far fa-bell"></i> Input with
+                        warning</label>
+                    <input type="text" class="form-control is-warning" id="inputWarning" placeholder="Enter ...">
+                </div>
+                <div class="form-group">
+                    <label class="col-form-label" for="inputError"><i class="far fa-times-circle"></i> Input with
+                        error</label>
+                    <input type="text" class="form-control is-invalid" id="inputError" placeholder="Enter ...">
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- checkbox -->
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <label class="form-check-label">Checkbox</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" checked>
+                                <label class="form-check-label">Checkbox checked</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" disabled>
+                                <label class="form-check-label">Checkbox disabled</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <!-- radio -->
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radio1">
+                                <label class="form-check-label">Radio</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radio1" checked>
+                                <label class="form-check-label">Radio checked</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" disabled>
+                                <label class="form-check-label">Radio disabled</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- select -->
+                        <div class="form-group">
+                            <label>Select</label>
+                            <select class="form-control">
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Select Disabled</label>
+                            <select class="form-control" disabled>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- Select multiple-->
+                        <div class="form-group">
+                            <label>Select Multiple</label>
+                            <select multiple class="form-control">
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label>Select Multiple Disabled</label>
+                            <select multiple class="form-control" disabled>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- /.card-body -->
     </div>
-    <strong>
-        <a href="{{ config('app.company_url', '#') }}">
-            {{ config('app.company_name', 'My company') }}
-        </a>
-    </strong>
 @stop
 
-{{-- Add common Javascript/Jquery code --}}
-@push('js')
-    <script>
-        $(document).ready(function () {
-            // Add your common script logic here...
-        });
-    </script>
-@endpush
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
 
-{{-- Add common CSS customizations --}}
-@push('css')
-    <style type="text/css">
-        {{-- You can add AdminLTE customizations here --}}
-
-        /*
-         .card-header {
-         border-bottom: none;
-         }
-         .card-title {
-         font-weight: 600;
-         }
-         */
-    </style>
-@endpush
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
